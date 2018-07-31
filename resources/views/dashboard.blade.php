3049d2b7aeaf606cb21ea58414554fddb88a5790
RumeshD <rumesh@witellsolutions.com>
2018-07-31 17:42:48 +0530

    first commot

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+  @extends('layouts.master')
+
+  @section('style')
+  @endsection
+
+  @section('content')
+
+
+  <section class="content-header">
+    <h1>
+      Dashboard
+      <small>Control panel</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Dashboard</li>
+    </ol>
+  </section>
+
+
+  <section class="content">
+
+
+  </section>
+
+</div>
+
+@endsection
---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,92 @@
+
+<aside class="main-sidebar">
+
+  <section class="sidebar">
+
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header">MAIN NAVIGATION</li>
+
+      <li class="active">
+        <a href="#">
+          <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+        </a>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-users"></i> <span>User Management</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+
+          <li class="treeview">
+            <a href=""> 
+              <i class="fa fa-angle-double-right"></i> <span> Permissions </span> 
+              <i class="fa fa-angle-right pull-right"></i>
+            </a>
+            <ul class="treeview-menu">
+              <li>
+                <a href="">
+                  <i class="fa fa-angle-double-right"></i>
+                  <span>Permission Add</span>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i class="fa fa-angle-double-right"></i>
+                  <span>Permission List</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="treeview">
+            <a href=""> 
+              <i class="fa fa-angle-double-right"></i> <span> Roles </span> 
+              <i class="fa fa-angle-right pull-right"></i>
+            </a>
+            <ul class="treeview-menu">
+              <li>
+                <a href="">
+                  <i class="fa fa-angle-double-right"></i>
+                  <span>Role Add</span>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i class="fa fa-angle-double-right"></i>
+                  <span>Role List</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+         <li class="treeview">
+            <a href=""> 
+              <i class="fa fa-angle-double-right"></i> <span> Users </span> 
+              <i class="fa fa-angle-right pull-right"></i>
+            </a>
+            <ul class="treeview-menu">
+              <li>
+                <a href="">
+                  <i class="fa fa-angle-double-right"></i>
+                  <span>User Add</span>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i class="fa fa-angle-double-right"></i>
+                  <span>User List</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+        </ul>
+      </li>
+    </ul>
+  </section>
+
+</aside>
